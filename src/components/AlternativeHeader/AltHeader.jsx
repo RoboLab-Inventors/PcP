071fe3975c5e9996929619abb57293919694416d
Ade <adepadova.ita@gmail.com>
2024-12-11 17:43:47 +0100

    Header Desktop and Tablet rensponsive, animation for y translation of the header based on scroll patterns

--- src/components/AlternativeHeader/AltHeader.jsx
+++ src/components/AlternativeHeader/AltHeader.jsx
@@ -1,8 +1,38 @@
 import React, { useState, useRef } from "react";
 import "./AltHeader.css";
-import logo from "../../assets/logo.png";
+import { useEffect } from "react";
+import logo from "../../assets/logoHeader.png";
 
 const AltHeader = () => {
+  const [showHeader, setShowHeader] = useState(true);
+  const lastScrollPos = useRef(0); // Use useRef to track last scroll position
+
+  // Function to handle scroll behavior
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+
+    if (currentScrollPos > lastScrollPos.current && currentScrollPos > 300) {
+      // Scrolling down - hide the header
+      setShowHeader(false);
+    } else if (currentScrollPos < lastScrollPos.current) {
+      // Scrolling up - show the header
+      setShowHeader(true);
+    }
+
+    // Update the last scroll position
+    lastScrollPos.current = currentScrollPos; // Update the ref
+  };
+
+  useEffect(() => {
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup: Remove the event listener when the component unmounts
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []); // Empty array to ensure it runs only once on mount
+
+  /*---------------------------------------------------------------------------------*/
+
   const [hoveredTab, setHoveredTab] = useState(null); // Stato per gestire il tab hoverato
   const tabs = ["Lessons", "Profiler", "Logo", "About Us", "Community"];
   const radioRefs = useRef([]); // Array per memorizzare i riferimenti agli input radio
@@ -24,9 +54,11 @@ const AltHeader = () => {
     }
     return 2; // Imposta "Logo" come predefinito selezionato (indice 2)
   };
+  const translateXValue =
+    (hoveredTab !== null ? hoveredTab : getCheckedTabIndex()) * 100;
 
   return (
-    <div className="container">
+    <div className={`container ${showHeader ? "visible" : "hidden"}`}>
       <div className="tabs">
         {tabs.map((tab, index) => (
           <React.Fragment key={index}>
@@ -58,7 +90,7 @@ const AltHeader = () => {
         <span
           className="glider"
           style={{
-            transform: `translateX(${(hoveredTab !== null ? hoveredTab : getCheckedTabIndex()) * 100}%)`,
+            transform: `translateX(${translateXValue}%)`,
           }}
         ></span>
       </div>
