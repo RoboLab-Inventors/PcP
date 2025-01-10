b6574014d8b905681876877c78c6b71736e27240
iTzEmany <manu.lionetti03@gmail.com>
2025-01-10 18:02:31 +0100

    Merge fix

---
+++ src/components/ToolGrid/EditComponent/EditComponent.jsx
@@ -0,0 +1,16 @@
+import './EditComponent.css';
+import '../ToolGrid.css';
+
+
+const EditComponent = () => {
+    return (
+        <div>
+            <h2>Edit Component</h2>
+            <div className="content">
+                <p>Edit options for the component.</p>
+            </div>
+        </div>
+    );
+};
+
+export default EditComponent;
---
+++ src/components/ToolGrid/ListController/ListController.jsx
@@ -0,0 +1,15 @@
+import './ListController.css';
+import '../ToolGrid.css';
+
+const ListController = () => {
+    return (
+        <div>
+            <h2>List Controller</h2>
+            <div className="content">
+                <p>Control the list of items here.</p>
+            </div>
+        </div>
+    );
+};
+
+export default ListController;
