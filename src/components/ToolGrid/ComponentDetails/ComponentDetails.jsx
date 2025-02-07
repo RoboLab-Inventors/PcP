b3fdfd32adb9d807d9ba0f42fdf5bbeceeb51743
iTzEmany <manu.lionetti03@gmail.com>
2025-02-07 20:31:36 +0100

    Tool Charts initialized

--- src/components/ToolGrid/ComponentDetails/ComponentDetails.jsx
+++ src/components/ToolGrid/ComponentDetails/ComponentDetails.jsx
@@ -1,13 +1,26 @@
+import React from "react";
+import Chart from "./Chart/Chart";
 import "./ComponentDetails.css";
 import "../ToolGrid.css";
 
-const ComponentDetails = () => {
+const ComponentDetails = ({ chartType }) => {
+  const getChart = () => {
+    switch (chartType) {
+      case "AxisChart":
+        return <Chart length={50} minValue={-255} maxValue={256} minY={-350} maxY={350} />;
+      case "SimpleButtonChart":
+        return <Chart length={30} minValue={0} maxValue={2} minY={-0.1} maxY={1.1} />;
+      case "ThreeWayButtonChart":
+        return <Chart length={30} minValue={0} maxValue={3} minY={-0.1} maxY={2.1} />;
+      default:
+        return <p>No chart available for the selected type.</p>;
+    }
+  };
+
   return (
-    <div>
-      <h2>Component Details</h2>
-      <div className="content">
-        {/* Add your content here */}
-        <p>Details about the component.</p>
+    <div className="component-container">
+      <div className="component-content">
+        {getChart()}
       </div>
     </div>
   );
