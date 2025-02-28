cefc23d28463dd8335057224a84032bfb2572bbc
Ade <adepadova.ita@gmail.com>
2025-02-28 19:15:05 +0100

    New searchbar style for community, still needs to be fixed

--- Front/src/components/SearchBar/SearchBar.jsx
+++ Front/src/components/SearchBar/SearchBar.jsx
@@ -2,24 +2,94 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import "./SearchBar.css";
+import Box from "@mui/material/Box";
+import PropTypes from "prop-types";
 
 const SearchBar = ({ setSearchQuery }) => (
   <form>
-    <TextField
-      id="search-bar"
-      className="text"
-      onInput={(e) => {
-        setSearchQuery(e.target.value);
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        width: "90vh",
+        position: "relative",
       }}
-      label="Cerca Configurazione"
-      variant="outlined"
-      placeholder="Cerca Configurazione..."
-      size="small"
-    />
-    <IconButton type="submit" aria-label="search">
-      <SearchIcon style={{ fill: "blue" }} />
-    </IconButton>
+    >
+      <IconButton
+        type="submit"
+        aria-label="search"
+        sx={{
+          color: "customTextColor.secondary",
+          border: "1px solid #fa1e4e",
+          "&:hover": {
+            boxShadow: "0px 0px 15px #fa1e4e",
+            backgroundColor: "#000000",
+          },
+          borderRadius: "5px",
+          height: "3.5rem",
+          margin: 1,
+          backgroundColor: "#ffffff",
+          borderTopRightRadius: 0,
+          borderBottomRightRadius: 0,
+          padding: "0.5rem", // Padding per allineare il contenuto
+          marginRight: "-1px",
+          borderTop: "1px solid #e7edf1",
+          borderLeft: "1px solid #e7edf1",
+          borderBottom: "1px solid #e7edf1",
+          borderRight: 0,
+        }}
+      >
+        <SearchIcon
+          style={{ fill: "customTextColor.secondary", fontSize: "2rem" }}
+        />
+      </IconButton>
+      <TextField
+        id="search-bar"
+        className="text"
+        onInput={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        label="Cerca Configurazione"
+        placeholder="Cerca Configurazione..."
+        size="small"
+        variant="outlined"
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "customTextColor.secondary",
+              borderTopLeftRadius: 0,
+              borderBottomLeftRadius: 0,
+              borderLeft: 0,
+            },
+            "&:hover fieldset": {
+              borderColor: "customTextColor.secondary",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "customTextColor.secondary",
+            },
+            "& input": {
+              color: "customTextColor.secondary",
+              height: "3.5rem",
+              fontSize: "1rem",
+              padding: "10px 14px",
+              borderLeft: 0,
+              boxSizing: "border-box",
+            },
+          },
+          "& .MuiInputLabel-outlined": {
+            color: "customTextColor.secondary",
+          },
+          "& .MuiInputLabel-outlined.Mui-focused": {
+            color: "customTextColor.secondary",
+          },
+          width: "100%",
+        }}
+      />
+    </Box>
   </form>
 );
+SearchBar.propTypes = {
+  setSearchQuery: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
--- Front/src/pages/Community/Community.jsx
+++ Front/src/pages/Community/Community.jsx
@@ -1,88 +1,91 @@
+import { useState, useEffect } from "react";
 import "./Community.css";
 import HeaderSwitch from "../../components/HeaderSwitch/HeaderSwitch";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import Grid from "@mui/material/Grid2";
 import ConfigurationCard from "../../components/ConfigurationCard/ConfigurationCard";
+import { BASE_URL } from "../../constants";
 
 function Community() {
+  const [configurations, setConfigurations] = useState([]);
+  const [filteredConfigurations, setFilteredConfigurations] = useState([]);
+  const [searchString, setSearchString] = useState(""); // Stato per la stringa di ricerca
+  const [selectedConfig, setSelectedConfig] = useState(null); // Stato per la configurazione selezionata
+
+  useEffect(() => {
+    async function getConfigurationList() {
+      try {
+        const response = await fetch(`${BASE_URL}/getCommunityConfiguration`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const data = await response.json();
+        setConfigurations(data.data);
+        setFilteredConfigurations(data.data); // Mostra tutte le configurazioni inizialmente
+      } catch (error) {
+        console.error("Error fetching configurations:", error);
+      }
+    }
+    getConfigurationList();
+  }, []);
+
+  const handleFilterConfigurations = (searchString) => {
+    setSearchString(searchString); // Aggiorna la stringa di ricerca
+    const filtered = configurations.filter(
+      (config) =>
+        config.nome.toLowerCase().includes(searchString.toLowerCase()) ||
+        config.descrizione.toLowerCase().includes(searchString.toLowerCase())
+    );
+    setFilteredConfigurations(filtered);
+  };
+
+  const highlightSubstring = (text) => {
+    if (!searchString) return text; // Se non c'è una stringa di ricerca, ritorna il testo normale
+
+    const parts = text.split(new RegExp(`(${searchString})`, "gi")); // Dividi il testo in parti, separando la sottostringa
+    return parts.map((part, index) =>
+      part.toLowerCase() === searchString.toLowerCase() ? (
+        <mark key={index}>{part}</mark> // Evidenzia la sottostringa
+      ) : (
+        part // Mantieni le altre parti come sono
+      )
+    );
+  };
+
   return (
     <>
       <HeaderSwitch />
 
       <div className="searchbarContainer">
-        <SearchBar />
+        <SearchBar
+          onChange={(e) => handleFilterConfigurations(e.target.value)}
+        />
       </div>
       <div className="gridContainer">
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
-        <div className="item">
-          <ConfigurationCard />
-        </div>
+        {filteredConfigurations.map((config) => (
+          <div className="item" key={config.idConfigurazione}>
+            <ConfigurationCard
+              idConfigurazione={config.idConfigurazione}
+              nome={config.nome}
+              descrizione={highlightSubstring(config.descrizione)}
+              username={config.username} // Passa l'username come prop
+            />
+          </div>
+        ))}
       </div>
-      <div></div>
-      {/*
 
-<Grid container spacing={4} alignItems="start">
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-            <ConfigurationCard />
-          </Grid>
-        </Grid>
-
-
-      <div
-        style={{
-          display: "flex",
-          alignSelf: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-          padding: 20,
-        }}
-      >
-        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-        <div style={{ padding: 3 }}>
-          {dataFiltered.map((d) => (
-            <div
-              className="text"
-              style={{
-                padding: 5,
-                justifyContent: "normal",
-                fontSize: 20,
-                color: "blue",
-                margin: 1,
-                width: "250px",
-                BorderColor: "green",
-                borderWidth: "10px",
-              }}
-              key={d.id}
-            >
-              {d}
-            </div>
-          ))}
+      {selectedConfig && (
+        <div className="selectedConfigDetails">
+          <h2>{selectedConfig.nome}</h2>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: highlightSubstring(selectedConfig.descrizione), // Evidenzia la sottostringa nella descrizione
+            }}
+          />
         </div>
-      </div>
-      */}
+      )}
     </>
   );
 }
