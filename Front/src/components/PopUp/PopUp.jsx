abedf1e3ed509ec43a525bd5bb1efa9a2f061e64
Ade <adepadova.ita@gmail.com>
2025-03-01 17:40:21 +0100

    PopUp done and parametrized

--- Front/src/components/PopUp/PopUp.jsx
+++ Front/src/components/PopUp/PopUp.jsx
@@ -4,14 +4,23 @@ import PropTypes from "prop-types";
 import { Typography } from "@mui/material";
 import CustomButton from "../CustomButton/CustomButton";
 
-const PopUp = ({ title, message, onClose }) => {
+const PopUp = ({
+  title,
+  message,
+  onCloseYes,
+  onCloseNo,
+  yesLabel,
+  noLabel,
+}) => {
   return ReactDOM.createPortal(
     <div className="popup">
       <div className="popup-inner">
         <Typography variant="h1">{title}</Typography>
         <Typography>{message}</Typography>
-        <CustomButton onClick={onClose} label="Procedi" />
-        <CustomButton onClick={onClose} label="Cancella" />
+        <div className="popup-buttons">
+          <CustomButton onClick={onCloseYes} label={yesLabel} />
+          <CustomButton onClick={onCloseNo} label={noLabel} />
+        </div>
       </div>
     </div>,
     document.body // Render the popup at the root level
